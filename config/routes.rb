b2a6da7c0b11e32dbae4f2574c37a221dfb2e1b6
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-12-10 13:46:37 -0300

    feat: add route for messages#create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get "hangouts/index"
 
+  resources :messages, only: [ :create ]
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
