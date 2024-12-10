4f95008f4079f5e0533e0d65e0024770ecf2e4ca
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-12-10 14:40:31 -0300

    feat: replace the form with rails helpers

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -1,6 +1,6 @@
 import consumer from "channels/consumer";
 
-const messageChannel = consumer.subscriptions.create("MessageChannel", {
+consumer.subscriptions.create("MessageChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -24,24 +24,4 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
               </div>
             </article>`;
   },
-});
-
-document.addEventListener("turbo:load", () => {
-  let form = document.querySelector("#message-form");
-
-  if (form) {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-
-      let messageInput = document.querySelector("#message-input").value;
-
-      if (messageInput == "") return;
-
-      const message = {
-        body: messageInput,
-      };
-
-      messageChannel.send({ message: message });
-    });
-  }
 });
